7e0179ac3fee3d7e67e24ac80d320b34c0ae42f9
ismail <ismail878708@gmail.com>
2023-06-06 21:12:57 +0600

    navbar is complete

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import NavigationBar from './Component/NavigationBar/NavigationBar'
+import Footer from './Component/Footer/Footer'
+
+function App() {
+ 
+
+  return (
+    <>
+    <NavigationBar></NavigationBar>
+    <Outlet></Outlet>
+    <Footer></Footer>
+      
+    </>
+  )
+}
+
+export default App
